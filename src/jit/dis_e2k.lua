1e1ba00eb79040c87f185610e1627d68c8ec85a0
helce <helce@bk.ru>
2026-01-27 12:04:09 +0300

    printing halfwords, change formatting, refactor print_als, ales decoding, more opcodes

--- src/jit/dis_e2k.lua
+++ src/jit/dis_e2k.lua
@@ -25,9 +25,39 @@ local map_op = {
 -- opc        name        format
   [0x00] = { "ands",     "ALOPF1" },
   [0x01] = { "andd",     "ALOPF1" },
+  [0x02] = { "andns",    "ALOPF1" },
+  [0x03] = { "andnd",    "ALOPF1" },
+  [0x04] = { "ors",      "ALOPF1" },
+  [0x05] = { "ord",      "ALOPF1" },
+  [0x06] = { "orns",     "ALOPF1" },
+  [0x07] = { "ornd",     "ALOPF1" },
+  ----------------------------------
+  [0x08] = { "xors",     "ALOPF1" },
+  [0x09] = { "xord",     "ALOPF1" },
+  [0x0a] = { "xorns",    "ALOPF1" },
+  [0x0b] = { "xornd",    "ALOPF1" },
+  [0x0c] = { "sxt",      "ALOPF1" },
+  [0x0e] = { "merges",   "ALOPF1" },
+  [0x0f] = { "merged",   "ALOPF1" },
+  ----------------------------------
   [0x10] = { "adds",     "ALOPF1" },
   [0x11] = { "addd",     "ALOPF1" },
+  [0x12] = { "subs",     "ALOPF1" },
+  [0x13] = { "subd",     "ALOPF1" },
+  [0x14] = { "scls",     "ALOPF1" },
+  [0x15] = { "scld",     "ALOPF1" },
+  [0x16] = { "scrs",     "ALOPF1" },
+  [0x17] = { "scrd",     "ALOPF1" },
+  ----------------------------------
+  [0x18] = { "shls",     "ALOPF1" },
+  [0x19] = { "shld",     "ALOPF1" },
+  [0x1a] = { "shrs",     "ALOPF1" },
+  [0x1b] = { "shrd",     "ALOPF1" },
+  [0x1c] = { "sars",     "ALOPF1" },
   [0x1d] = { "sard",     "ALOPF1" },
+  [0x1e] = { "getfs",    "ALOPF1" },
+  [0x1f] = { "getfd",    "ALOPF1" },
+  ----------------------------------
   [0x20] = { "cmpsb",    "ALOPF7" },
   [0x21] = { "cmpdb",    "ALOPF7" },
   [0x22] = { "cmpandsb", "ALOPF7" },
@@ -50,6 +80,26 @@ local map_op = {
   [0x3f] = { "fdtos",    "ALOPF2" },
   ----------------------------------
   [0x67] = { "ldd",      "ALOPF1" },
+}
+
+local map_opext = {
+-- opc2       opc        name      format    opce2
+  [0x01] = {
+             [0x20] = { "muls",    "ALOPF11", 0xc0 },
+             [0x21] = { "muld",    "ALOPF11", 0xc0 },
+             [0x22] = { "umulx",   "ALOPF11", 0xc0 },
+             [0x23] = { "smulx",   "ALOPF11", 0xc0 },
+             [0x49] = { "fdivd",   "ALOPF11", 0xc0 },
+             [0x4d] = { "fsqrtid", "ALOPF12", 0xc0 }, -- ignore opce
+             [0x51] = { "fsqrttd", "ALOPF11", 0xc0 },
+             [0x58] = { "getsp",   "ALOPF12", 0xc0 }, -- ignore opce
+             [0x6d] = { "fdtoifd", "ALOPF11", 0xc0 },
+  },
+-- PFCMB1
+  [0x0f] = {
+             [0x4d] = { "pshufb",  "ALOPF21", 0x00 },
+
+  },
 }
 
 local map_fsd = {
@@ -92,6 +142,18 @@ local map_cs0 = {
  [3] = { "disp",    nil,     "sdisp",  "return" },
 }
 
+local function get_halfword(ctx, pos)
+  if ctx.half_hi then
+    local b3, b4 = byte(ctx.code, pos+3, pos+4)
+    ctx.half_hi = false
+    return bor(lshift(b4, 8), b3)
+  else
+    local b0, b1 = byte(ctx.code, pos+1, pos+2)
+    ctx.half_hi = true
+    return bor(lshift(b1, 8), b0)
+  end
+end
+
 local function get_word(ctx, pos)
   local b0, b1, b2, b3 = byte(ctx.code, pos+1, pos+4)
   return bor(lshift(b3, 24), lshift(b2, 16), lshift(b1, 8), b0) 
@@ -112,7 +174,7 @@ local function print_src1(ctx, src1)
   end
 end
 
-local function print_src2(ctx, src2, lts_pos)
+local function print_src2(ctx, src2)
   if band(src2, 0x80) == 0 then
     return format("%%b%d", band(src2, 0x7f))
   elseif band(src2, 0xc0) == 0x80 then
@@ -123,7 +185,7 @@ local function print_src2(ctx, src2, lts_pos)
     return format("0x%x", band(src2, 0x0f))
   elseif band(src2, 0xf8) == 0xd0 then
     local lts_n = band(src2, 0x3)
-    local code = ctx:get(lts_pos - lshift(lts_n + 1, 2))
+    local code = ctx:get(ctx.lts_pos - lshift(lts_n + 1, 2))
     if band(src2, 0x4) == 0 then
       return format("lts%d_lo 0x%x", lts_n, band(code, 0xffff))
     else
@@ -131,13 +193,13 @@ local function print_src2(ctx, src2, lts_pos)
     end
   elseif band(src2, 0xfc) == 0xd8 then
     local lts_n = band(src2, 0x3)
-    local code = ctx:get(lts_pos - lshift(lts_n + 1, 2))
+    local code = ctx:get(ctx.lts_pos - lshift(lts_n + 1, 2))
     return format("lts%d 0x%x", lts_n, code)
   elseif band(src2, 0xfc) == 0xdc then
     local lts_n = band(src2, 0x3)
-    local code_lo = ctx:get(lts_pos - lshift(lts_n + 1, 2))
-    local code_hi = ctg:get(lts_pos - lshift(lts_n + 2, 2))
-    return format("lts%d-%d 0x%x%x", lts_n, lts_n+1, code_hi, code_lo)
+    local code_lo = ctx:get(ctx.lts_pos - lshift(lts_n + 1, 2))
+    local code_hi = ctx:get(ctx.lts_pos - lshift(lts_n + 2, 2))
+    return format("lts%d-%d 0x%x%08x", lts_n, lts_n+1, code_hi, code_lo)
   else
     error("unrecognized src2")
   end
@@ -175,69 +237,108 @@ local function print_pdst(ctx, pdst)
   return format("%%pred%d", pdst)
 end
 
+local function print_alf1(ctx, code)
+  local src1 = print_src1(ctx, band(rshift(code, 16), 0xff))
+  local src2 = print_src2(ctx, band(rshift(code, 8), 0xff))
+  local dst  = print_dst(ctx, band(code, 0xff))
+  return src1, src2, dst
+end
+
+local function print_alf2(ctx, code)
+  local opce = band(rshift(code, 16), 0xff)
+  local src2 = print_src2(ctx, band(rshift(code, 8), 0xff))
+  local dst  = print_dst(ctx, band(code, 0xff))
+  return opce, src2, dst
+end
+
+local function print_alf3(ctx, code)
+  local src1 = print_src1(ctx, band(rshift(code, 16), 0xff))
+  local src2 = print_src2(ctx, band(rshift(code, 8), 0xff))
+  local src3 = print_src3(ctx, band(code, 0xff))
+  return src1, src2, src3
+end
+
+local function print_alf7(ctx, code)
+  local src1 = print_src1(ctx, band(rshift(code, 16), 0xff))
+  local src2 = print_src2(ctx, band(rshift(code, 8), 0xff))
+  local opce = band(rshift(code, 5), 0x7)
+  local pdst = print_pdst(ctx, band(code, 0x1f))
+  return src1, src2, opce, pdst
+end
+
+local function print_alef2(ctx, code)
+  local opc2 = band(rshift(code, 8), 0xff)
+  local opce2 = band(code, 0xff)
+  return opc2, opce2
+end
+
 -- Output ALU operations.
 local function print_als(ctx)
-  local pos, als, lts_pos = ctx.als_pos, ctx.als, ctx.lts_pos
-  local op, als_n = "", 0
+  local als, als_pos = ctx.als, ctx.als_pos
+  local ales, ales_pos = ctx.ales, ctx.ales_pos
+  local als_n = 0
+  ctx.half_hi = true -- start with hi
   while als ~= 0 do
-    if band(als, 1) then
-      local code = ctx:get(pos)
+    local name, ops = nil, nil
+    if band(als, 1) ~= 0 then
+      local code = ctx:get(als_pos)
       local spec = band(rshift(code, 31), 0x1)
       local cop = band(rshift(code, 24), 0x7f)
-      if map_op[cop] then
-        local fmt = map_op[cop][2]
-        if fmt == "ALOPF1" then
-          local src1 = print_src1(ctx, band(rshift(code, 16), 0xff))
-          local src2 = print_src2(ctx, band(rshift(code, 8), 0xff), lts_pos)
-          local dst  = print_dst(ctx, band(code, 0xff))
-          op = map_op[cop][1]
-          if spec == 1 then op = op..",sm" end
-          op = format("%s %s, %s, %s", op, src1, src2, dst)
-        elseif fmt == "ALOPF2" then
-          local opce = band(rshift(code, 16), 0xff)
-          local src2 = print_src2(ctx, band(rshift(code, 8), 0xff), lts_pos)
-          local dst  = print_dst(ctx, band(code, 0xff))
-          op = map_op[cop][1]
-          if     op == "fstos" then op = map_fsd[opce][1]
-          elseif op == "fstod" then op = map_fsd[opce][2]
-          elseif op == "fdtos" then op = map_fsd[opce][3]
-          elseif op == "fdtod" then op = map_fsd[opce][4]
+      if band(ales, 1) ~= 0 then
+        local ales_code = ctx:geth(ales_pos)
+        local opc2 = band(rshift(ales_code, 8), 0xff)
+        if map_opext[opc2] and map_opext[opc2][cop] then
+          local fmt = map_opext[opc2][cop][2]
+          if fmt == "ALOPF11" then
+          elseif fmt == "ALOPF12" then
+            local opce, src2, dst = print_alf2(ctx, code)
+            local opc2, opce2 = print_alef2(ctx, ales_code)
+            name = map_opext[opc2][cop][1]
+            ops = format("%s, %s", src2, dst)
           end
-          if spec == 1 then op = op..",sm" end
-          op = format("%s %s, %s", op, src2, dst)
-        elseif fmt == "ALOPF3" then
-          local src1 = print_src1(ctx, band(rshift(code, 16), 0xff))
-          local src2 = print_src2(ctx, band(rshift(code, 8), 0xff), lts_pos)
-          local src3 = print_src3(ctx, band(code, 0xff))
-          op = map_op[cop][1]
-          if spec == 1 then op = op..",sm" end
-          op = format("%s %s, %s, %s", op, src1, src2, src3)
-        elseif fmt == "ALOPF7" then
-          local src1 = print_src1(ctx, band(rshift(code, 16), 0xff))
-          local src2 = print_src2(ctx, band(rshift(code, 8), 0xff), lts_pos)
-          local opce = band(rshift(code, 5), 0x7)
-          local pdst = print_pdst(ctx, band(code, 0x1f))
-          op = map_op[cop][1]
-          if     op == "cmpsb" then op = map_cmp[opce][1]
-          elseif op == "cmpdb" then op = map_cmp[opce][2]
-          elseif op == "cmpandsb" then op = map_cmp[opce][3]
-          elseif op == "cmpanddb" then op = map_cmp[opce][4]
-          elseif op == "fcmpsb" then op = map_cmp[opce][5]
-          elseif op == "fcmpdb" then op = map_cmp[opce][6]
-          end
-          if spec == 1 then op = op..",sm" end
-          op = format("%s %s, %s, %s", op, src1, src2, pdst)
         end
+        if ctx.half_hi then ales_pos = ales_pos + 4 end
       else
-        op = "unrecognized"
+        if map_op[cop] then
+          local fmt = map_op[cop][2]
+          if fmt == "ALOPF1" then
+            name = map_op[cop][1]
+            ops = format("%s, %s, %s", print_alf1(ctx, code))
+          elseif fmt == "ALOPF2" then
+            local opce, src2, dst = print_alf2(ctx, code)
+            if cop >= 0x3c and cop <= 0x3f then
+              name = map_fsd[opce][cop - 0x3c + 1]
+            else
+              name = map_op[cop][1]
+            end
+            ops = format("%s, %s", src2, dst)
+          elseif fmt == "ALOPF3" then
+            name = map_op[cop][1]
+            ops = format("%s, %s, %s", print_alf3(ctx, code))
+          elseif fmt == "ALOPF7" then
+            local src1, src2, opce, pdst = print_alf7(ctx, code)
+            if cop >= 0x20 and cop <= 0x23 then
+              name = map_cmp[opce][cop - 0x20 + 1]
+            elseif cop >= 0x2e and cop <= 0x2e then
+              name = map_cmp[opce][cop - 0x2e + 5]
+            else
+              name = map_op[cop][1]
+            end
+            ops = format("%s, %s, %s", src1, src2, pdst)
+          end
+        end
       end
-      ctx.out(format("alc%d:   %s\n", als_n, op))
+      if not name then name = "unrecognized" end
+      name = name..","..als_n
+      if spec == 1 then name = name..",sm" end
+      ctx.out(format("        %s %s\n", name, ops))
     end
     als_n = als_n + 1
-    pos = pos + 4
+    als_pos = als_pos + 4
     als = rshift(als, 1)
+    ales = rshift(ales, 1)
   end
-  return pos
+  return als_pos
 end
 
 -- Output control operations.
@@ -271,15 +372,15 @@ local function print_cs(ctx)
       local sym = ctx.symtab[ctx.addr + ctx.pos + disp]
       if op == "ibranch" then ct = false end
       if pred then p = format(", %s%%pred%d", inv, pred) end
-      ctx.out(format("cs0+ss: %s ->%s%s\n", op, sym, p))
+      ctx.out(format("        %s ->%s%s\n", op, sym, p))
     else
       if op == "sdisp" then
-        ctx.out(format("cs0:    %s %%ctpr%d, %s\n", op, ctpr, band(code, 0x1f)))
+        ctx.out(format("        %s %%ctpr%d, %s\n", op, ctpr, band(code, 0x1f)))
       elseif op == "return" then
-        ctx.out(format("cs0:    %s %%ctpr%d\n", op, ctpr))
+        ctx.out(format("        %s %%ctpr%d\n", op, ctpr))
       else
         local sym = ctx.symtab[ctx.addr + ctx.pos + disp]
-        ctx.out(format("cs0:    %s %%ctpr%d, %s\n", op, ctpr, sym))
+        ctx.out(format("        %s %%ctpr%d, %s\n", op, ctpr, sym))
       end
     end
     pos = pos + 4
@@ -298,8 +399,8 @@ end
 
 -- Disassemble  a single wide instruction.
 local function disass_ins(ctx)
-  local hex, ofs, pos = "", 0, ctx.pos
-  local hs = ctx:get(pos)
+  local hex, ofs= "", 0
+  local hs = ctx:get(ctx.pos)
   -- get hs fields
   ctx.als = band(rshift(hs, 26), 0x3f)
   ctx.ales = band(rshift(hs, 20), 0x3f)
@@ -312,17 +413,18 @@ local function disass_ins(ctx)
   ctx.mdl = band(hs, 0xf) + 1
 
   for i=0,ctx.lng-1 do
-    hex = hex.." "..tohex(ctx:get(pos + ofs))
+    hex = hex.." "..tohex(ctx:get(ctx.pos + ofs))
     ofs = ofs + 4
   end
 
   ctx.epos = ctx.pos + ofs
   if ctx.hexdump == 0 then hex = "" end
-  ctx.out(format("%08x: %s\n", ctx.addr + pos, hex))
+  ctx.out(format("%08x: %s\n", ctx.addr + ctx.pos, hex))
   -- its v3 style so ignoring ALES2/5 and cs1 right after cs0
-  ctx.ss_pos  = pos + 4
+  ctx.ss_pos  = ctx.pos + 4
   ctx.als_pos = ctx.ss_pos + lshift(ctx.ss, 2)
   ctx.cs_pos = ctx.als_pos
+  ctx.ales_pos = ctx.pos + lshift(ctx.mdl, 2)
   ctx.lts_pos = ctx.epos - lshift(ctx.cds + ctx.pl , 2)
   if ctx.als ~= 0 then
     ctx.cs_pos = print_als(ctx)
@@ -358,6 +460,8 @@ local function create(code, addr, out)
   ctx.hexdump = 16
   ctx.ct = nil
   ctx.get = get_word
+  ctx.geth = get_halfword
+  ctx.half_hi = true
   return ctx
 end
 
